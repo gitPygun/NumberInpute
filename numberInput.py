87e1184712a21aa1c62c566e361a88bcfb0788e5
gitPygun <54455223+gitPygun@users.noreply.github.com>
2021-10-01 16:46:54 -0400

    Add files via upload

---
+++ numberInput.py
@@ -0,0 +1,12 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+    
+
+a = int(input('Enter 1st number: ')) #There was an error here.
+b = int(input('Enter 2nd number: ')) #There was an error here.
+
+showPrint = f'Sum of {a and b} is {sum(a, b)}' #There was an error here.
+print(showPrint)
+
